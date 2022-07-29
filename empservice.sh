0fb1c84b8007b46b541d8137d1bebe4cb2cff324
kesav38 <kesavareddykesavareddy@gmail.com>
2022-07-29 17:22:47 +0530

    empdt

--- empservice.sh
+++ empservice.sh
@@ -1,1 +1,1 @@
- emplloyee service is very important thtas its soo easy for us to tackle this .so keep the emplyee service info as confidential as much as u can.
+ emplloyee service is very important thtas its soo easy for us to tackle this .so keep the emplyee service info as confidential as much as u can hence proved.
