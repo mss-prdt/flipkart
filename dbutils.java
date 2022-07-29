46a8017a9add5156a2c968947c5a5492d047e43d
kesav38 <kesavareddykesavareddy@gmail.com>
2022-07-29 16:16:35 +0530

    database & services added

---
+++ dbutils.java
@@ -0,0 +1,1 @@
+now a days database is very important to maintain the data of various app and various servers so its neccesary to keep it safe that why we need th cloud security.
---
+++ servies.java
@@ -0,0 +1,1 @@
+s similar to the services of java we need to think services are very important to kep the links alive.
