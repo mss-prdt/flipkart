e252759338a42b1a2c2319ee703acd98cf77b7af
kesav38 <kesavareddykesavareddy@gmail.com>
2022-08-03 11:58:57 +0530

    sevice updated

--- servies.java
+++ servies.java
@@ -1,1 +1,4 @@
 s similar to the services of java we need to think services are very important to kep the links alive. need to  keep the services as secret as possibl     n  c ncbnsbcnssce
+
+
+service store are very imortant among all over the india , its the primary service.
